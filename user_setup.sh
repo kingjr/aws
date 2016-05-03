12a70f0e35e6be2b01fe582d23f24184751b3256
Jean-Rémi KING <jeanremi.king@gmail.com>
2016-05-03 19:26:16 -0400

    Update user_setup.sh

--- user_setup.sh
+++ user_setup.sh
@@ -3,7 +3,7 @@
 wget http://repo.continuum.io/archive/Anaconda2-4.0.0-Linux-x86_64.sh  # check version
 bash Anaconda2-4.0.0-Linux-x86_64.sh # need manual "yes" responses
 rm Anaconda2-4.0.0-Linux-x86_64.sh
-source ~/.bashrc
+source /home/ubuntu/.bashrc
 
 # Install depencies
 conda install numpy setuptools numpy scipy matplotlib nose mayavi pandas h5py pil patsy pyside joblib
@@ -22,10 +22,10 @@ git clone https://github.com/kingjr/meg_perceptual_decision_symbols
 
 # Setup ipython profile
 ipython profile create
-echo "import matplotlib; matplotlib.use('Agg')" > ~/.ipython/profile_default/ipython_config.py
+echo "import matplotlib; matplotlib.use('Agg')" > /home/ubuntu/.ipython/profile_default/ipython_config.py
 
 # Setup jupyter notebook
 PASSWD=$(ipython -c "import IPython;pwd=IPython.lib.passwd();print(pwd)")
-jupyter notebook --generate-config
-cp ~/aws/jupyter_notebook_config.py ~/.jupyter/jupyter_notebook_config.py
-echo "c.NotebookApp.password = u'$PASSWD'" > ~/.jupyter/jupyter_notebook_config.py
+jupyter notebook --generate-config -y
+cp /home/ubuntu/aws/jupyter_notebook_config.py /home/ubuntu/.jupyter/jupyter_notebook_config.py
+echo "c.NotebookApp.password = u'$PASSWD'" > /home/ubuntu/.jupyter/jupyter_notebook_config.py
