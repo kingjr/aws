318d770bf10df3b28a56b8a7a04038fd5125cfb6
Jean-Rémi KING <jeanremi.king@gmail.com>
2016-05-04 16:34:00 -0400

    Update user_setup.sh

--- user_setup.sh
+++ user_setup.sh
@@ -2,13 +2,13 @@
 # Install depencies
 conda install numpy setuptools numpy scipy matplotlib nose mayavi pandas h5py pil patsy pyside joblib dropbox
 pip install nibabel
-pip install git+https://github.com/scikit-learn/scikit-learn
-pip install git+https://github.com/mne-tools/mne-python
-pip install git+https://github.com/mne-tools/mne-sandbox
-pip install git+https://github.com/dengemann/meeg-preprocessing
-pip install git+https://github.com/alexandrebarachant/pyRiemann
-pip install git+https://github.com/kingjr/jr-tools
-pip install git+https://github.com/h5io/h5io
+git clone https://github.com/scikit-learn/scikit-learn; cd scikit-learn; python setup.py install; cd ~/
+git clone https://github.com/mne-tools/mne-python; cd mne-python; python setup.py install; cd ~/
+git clone https://github.com/mne-tools/mne-sandbox; cd mne-sandbox; python setup.py install; cd ~/
+git clone https://github.com/dengemann/meeg-preprocessing; cd meeg-preprocessing; python setup.py install; cd ~/
+git clone https://github.com/alexandrebarachant/pyRiemann; cd pyRiemann; python setup.py install; cd ~/
+git clone https://github.com/kingjr/jr-tools; cd jr-tools; python setup.py install; cd ~/
+git clone https://github.com/h5io/h5io; cd h5io/; python setup.py install; cd ~/
 
 # Add working projects
 git clone https://github.com/kingjr/valentin-eeg
