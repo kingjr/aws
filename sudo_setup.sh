dffd48de348fcb21a62c92f37472d183910c3f8c
Jean-Rémi KING <jeanremi.king@gmail.com>
2016-05-03 23:42:58 -0400

    Update sudo_setup.sh

--- sudo_setup.sh
+++ sudo_setup.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-# Setup jupyter server at launch
-# ln /home/ubuntu/aws/update.sh /etc/init.d/  # symbolic link
-# chmod +x /etc/init.d/update.sh  # make it executable
-# update-rc.d update.sh defaults  # enable startup
-# update-rc.d update.sh start 20 2 3 4 5 . stop 20 0 1 6 .  # legacy mode
-cp /home/ubuntu/aws/run_at_launch.conf /etc/init/
-
 # Setup jupyter server ssl
 mkdir certs
 cd certs
