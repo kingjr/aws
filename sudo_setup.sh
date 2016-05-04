bb1780e5ddec5d492ce103f8def6395d4c5192dc
Jean-Rémi KING <jeanremi.king@gmail.com>
2016-05-04 00:51:18 -0400

    cloud init

--- sudo_setup.sh
+++ sudo_setup.sh
@@ -5,3 +5,6 @@ cd certs
 openssl req -x509 -nodes -days 365 -newkey rsa:1024 -keyout mycert.pem -out mycert.pem \
     -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=www.example.com"
 cd /home/ubuntu/
+
+# Cloud init XXX not working?
+cp /aws/user-data.txt /var/lib/cloud/data/
