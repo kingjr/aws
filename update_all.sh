37780b48aa112047caa9d84be183d37b44e2711b
Jean-Rémi KING <jeanremi.king@gmail.com>
2016-05-10 11:58:09 -0400

    Update update_all.sh
    add env variable in jupyter with update

--- update_all.sh
+++ update_all.sh
@@ -3,6 +3,9 @@
 echo 'export aws=True' >> /home/ubuntu/.bashrc
 source /home/ubuntu/.bashrc
 
+echo  'import os' >> /home/ubuntu/.jupyter/jupyter_notebook_config.py
+echo "os.environ['aws'] = 'True'" >> /home/ubuntu/.jupyter/jupyter_notebook_config.py
+
 # Update all
 conda update numpy setuptools numpy scipy matplotlib nose mayavi pandas h5py pil patsy pyside joblib dropbox -y
 pip install nibabel --upgrade
